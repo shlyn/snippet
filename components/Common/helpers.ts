fc226addc0b155fa180283d7786ab06e69a0dfdf
Ashlynn Rodriguez <arodriguez@deafbiblesociety.com>
2019-04-30 23:58:14 -0500

    initial commit

--- components/Common/helpers.ts
+++ components/Common/helpers.ts
@@ -4,8 +4,10 @@ function noop() {}
 
 export function preventDefault<E extends React.SyntheticEvent>(
   func: (event: E) => void = noop
+  
 ) {
   return (event: E) => {
+    console.log(event)
     event.preventDefault();
     func(event);
   };
